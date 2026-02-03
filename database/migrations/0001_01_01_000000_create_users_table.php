102d184fc233723df93db2b8142768549656ad57
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-02-03 16:08:04 +0100

    docs: la migration de tableau users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
