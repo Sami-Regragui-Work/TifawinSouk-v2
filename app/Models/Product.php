c30859c78f9f9d3770186cb7b23459bcda571ed7
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-06 12:18:22 +0100

    fix(models): rewrote the cart relation in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,8 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class, 'order_products', 'product_id', 'order_id')->withPivot('quantity', 'price_at_purchase');
     }
+    public function cartProducts()
+    {
+        return $this->belongsToMany(Cart::class, 'cart_products', 'product_id', 'cart_id')->withPivot('quantity');
+    }
 }
