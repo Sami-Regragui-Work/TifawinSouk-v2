b8a6de52fde7343ffc7ef17be17c4e483f73d9f5
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-02-06 11:30:42 +0100

    feat(models): rewrote the relation in order model and added 'total' method

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,12 +8,16 @@ class Order extends Model
 {
     protected $fillable = ['client_id'];
 
-    public function user()
+    public function client()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'client_id');
     }
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsToMany(Product::class, 'order_products', 'order_id', 'product_id')->withPivot('quantity', 'price_at_purchase');
+    }
+    public function total(): float
+    {
+        return $this->products->sum(fn($product) => $product->pivot->quantity * $product->pivot->price_at_purchase);
     }
 }
